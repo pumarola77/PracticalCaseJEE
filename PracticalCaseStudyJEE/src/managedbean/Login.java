843b3d102f26ef17a9adb06965f4b0548b35202f
xpumarola <xpumarola@uoc.edu>
2016-11-21 21:20:33 +0100

    Pantalla Login

---
+++ PracticalCaseStudyJEE/src/managedbean/Login.java
@@ -0,0 +1,16 @@
+package managedbean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "Login")
+@SessionScoped
+public class Login implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	
+	
+}
