06dd13fe0e288e4a2d9a8c4f69e93e8253a542d3
mperezclo <mperezclo@uoc.edu>
2016-11-30 01:11:33 +0100

    Afegir i eliminar idiomes (Language To Talk)

---
+++ PracticalCaseStudyJEE/src/managedbean/DeleteLanguageToTalkMBean.java
@@ -0,0 +1,38 @@
+package managedbean;
+
+import java.io.Serializable;
+import java.util.*;
+import javax.ejb.EJB;
+import javax.faces.bean.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+import ejb.UserFacadeRemote;
+
+/**
+ * Managed Bean DeleteLanguageToTalkMBean
+ */
+@ManagedBean(name = "languagetotalkdelete")
+@SessionScoped
+public class DeleteLanguageToTalkMBean implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@EJB
+	private UserFacadeRemote deleteLanguageToTalkRemote;
+
+	/**
+	 * Metode que es fa servir per eliminar un 'language To Talk'
+	 * @return Nom del Facelet
+	 * @throws Exception
+	 */
+	public String deleteLanguageToTalk(String nif, String language) throws Exception
+	{
+		Properties props = System.getProperties();
+		Context ctx = new InitialContext(props);
+		deleteLanguageToTalkRemote = (UserFacadeRemote) ctx.lookup("java:app/PracticalCaseStudyJEE.jar/UserFacadeBean!ejb.UserFacadeRemote");
+		deleteLanguageToTalkRemote.deleteLanguageToTalk(nif, language);
+
+		return "LanguagesToTalkListView";
+	}
+}
