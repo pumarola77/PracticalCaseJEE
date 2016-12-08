39fb5fac6c04d6bde0f8d101c3a8001ab8240124
Pol <pmartinezmartinez@uoc.edu>
2016-12-08 13:26:26 +0100

    Actualitzat LocationJPA amb ID auto-incremental

--- PracticalCaseStudyJEE/src/jpa/LocationJPA.java
+++ PracticalCaseStudyJEE/src/jpa/LocationJPA.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -17,6 +19,7 @@ public class LocationJPA implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Column(name = "id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	
 	@Column(name = "street")
