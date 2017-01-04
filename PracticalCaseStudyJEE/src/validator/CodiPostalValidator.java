d7938db41965d1f280fdc3d133d220326d202027
xpumarola <xpumarola@uoc.edu>
2017-01-04 22:19:08 +0100

    Controlar Validacions.

---
+++ PracticalCaseStudyJEE/src/validator/CodiPostalValidator.java
@@ -0,0 +1,43 @@
+package validator;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+@FacesValidator("CodiPostalValidator")
+public class CodiPostalValidator implements Validator {
+
+	public CodiPostalValidator() {
+		
+	}
+	
+	@Override
+	public void validate(FacesContext arg0, UIComponent component, Object object) throws ValidatorException {
+		// TODO Auto-generated method stub
+		
+		String codiPostal  = object.toString();
+		
+		if (codiPostal.matches(".*\\s+.*"))
+		{
+			FacesMessage msg = 	new FacesMessage("Validació del telèfon errònia.", "El codipostal no pot contenir espais en blanc");
+			msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+			throw new ValidatorException(msg);
+		}
+		
+		if (!codiPostal.matches("\\d+"))
+		{
+			FacesMessage msg = 	new FacesMessage("Validació del telèfon errònia.", "El codipostal únicament ha de contenir números");
+			msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+			throw new ValidatorException(msg);
+		}
+
+		
+	}
+	
+	
+
+}
